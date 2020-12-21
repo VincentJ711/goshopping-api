df9f40526616d3648a68bf6c24dda4ede455d1ec
hishamss <hishamss90@gmail.com>
2020-12-21 13:53:09 -0700

    use ControllerUtility handler in the login controller

--- src/main/java/com/revature/goshopping/controller/LoginController.java
+++ src/main/java/com/revature/goshopping/controller/LoginController.java
@@ -1,6 +1,9 @@
 package com.revature.goshopping.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,27 +14,19 @@ import com.revature.goshopping.model.Auth;
 import com.revature.goshopping.model.LoginForm;
 import com.revature.goshopping.model.LoginResponse;
 import com.revature.goshopping.service.LoginService;
+import com.revature.goshopping.utility.ControllerUtility;
 
 @RestController
 @RequestMapping("/login")
 public class LoginController {
-	
+
 	@Autowired
 	LoginService loginService;
-	
+
 	@PostMapping
-	public LoginResponse login(@RequestBody LoginForm loginForm) {
-		try {
-			LoginResponse loginResponse = loginService.login(loginForm.getUsername(), loginForm.getPassword());
-			return loginResponse;
-		}catch (ServiceException e) {
-			e.printStackTrace();
-			System.out.println(e.getMessage());
-			LoginResponse loginResponse = new LoginResponse(e.getMessage());
-			return loginResponse;
-		}
-		
-		
+	public ResponseEntity<LoginResponse> login(@RequestBody LoginForm loginForm) {
+		return ControllerUtility.handle(() -> loginService.login(loginForm.getUsername(), loginForm.getPassword()));
+
 	}
 
 }
--- src/main/java/com/revature/goshopping/service/LoginService.java
+++ src/main/java/com/revature/goshopping/service/LoginService.java
@@ -15,7 +15,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class LoginService {
 	
-	//@Autowired
+	@Autowired
   UserDaoForLoginService dao = new UserDaoForLoginService();
 
   /**
