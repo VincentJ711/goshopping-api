4a151d9a255f540ef62d7f2826284cec1c4c6ea6
VincentJ711 <vincentj711@gmail.com>
2020-12-28 15:51:39 -0800

    add api documentation. enhance ServiceException toString. add default constructor for SwapPassword for jackson. clean up order controller. remove need to do PUT /user/{id} since we're only changing password and therefore already know the id must be on the Auth for the request. have OrderEntity eagerly fetch order items. consolidate item searching to one method in item controller/service/dao. add quantity/page params for item searching. switch from persisting tag entity to saveOrUpdate(ing) in item dao to fix bug when trying to create an item with an existing tag and a new tag.

--- src/main/java/com/revature/goshopping/controller/UserController.java
+++ src/main/java/com/revature/goshopping/controller/UserController.java
@@ -1,30 +1,17 @@
 package com.revature.goshopping.controller;
 
-import java.util.List;
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.revature.goshopping.dto.Auth;
-import com.revature.goshopping.dto.LoginForm;
-import com.revature.goshopping.dto.LoginResponse;
 import com.revature.goshopping.dto.SwapPassword;
 import com.revature.goshopping.dto.User;
 import com.revature.goshopping.service.UserService;
 import com.revature.goshopping.utility.ControllerUtility;
 import com.revature.goshopping.utility.JwtUtility;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/user")
@@ -69,7 +56,7 @@ public class UserController {
 		});
 	}
 
-	@PutMapping("/{id}")
+	@PutMapping("")
 	public ResponseEntity<User> updatePassword(@RequestHeader Map<String, String> headers,
 			@RequestBody SwapPassword newPass) {
 		Auth auth = JwtUtility.getAuth(headers);
--- src/main/java/com/revature/goshopping/entity/OrderEntity.java
+++ src/main/java/com/revature/goshopping/entity/OrderEntity.java
@@ -1,12 +1,6 @@
 package com.revature.goshopping.entity;
 
-import com.revature.goshopping.dto.Order;
-
 import javax.persistence.*;
-
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
-
 import java.sql.Date;
 import java.util.HashSet;
 import java.util.Set;
@@ -14,64 +8,64 @@ import java.util.Set;
 @Entity
 @Table(name = "orders")
 public class OrderEntity {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "order_id")
-	private int id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "order_id")
+  private int id;
 
-	private Date date;
+  private Date date;
 
-	@OneToMany(mappedBy = "order", cascade = { CascadeType.REMOVE, CascadeType.PERSIST })
-	private Set<ItemOrderEntity> itemOrders = new HashSet<>();
+  @OneToMany(mappedBy = "order", cascade = {CascadeType.REMOVE,
+      CascadeType.PERSIST}, fetch = FetchType.EAGER)
+  private Set<ItemOrderEntity> itemOrders = new HashSet<>();
 
+  @ManyToOne(cascade = CascadeType.REMOVE)
+  @JoinColumn(name = "user_id")
+  private UserEntity user;
 
-	@ManyToOne( cascade = CascadeType.REMOVE)
-	@JoinColumn(name = "user_id")
-	private UserEntity user;
+  public OrderEntity() {
+    super();
+  }
 
-	public OrderEntity() {
-		super();
-	}
+  public OrderEntity(Date date, UserEntity user) {
+    super();
+    this.date = date;
+    this.user = user;
+  }
 
-	public OrderEntity(Date date, UserEntity user) {
-		super();
-		this.date = date;
-		this.user = user;
-	}
+  public void addItem(ItemEntity item, int quantity) {
+    itemOrders.add(new ItemOrderEntity(item, this, quantity));
+  }
 
-	public void addItem(ItemEntity item, int quantity) {
-		itemOrders.add(new ItemOrderEntity(item, this, quantity));
-	}
+  public int getId() {
+    return id;
+  }
 
-	public int getId() {
-		return id;
-	}
+  public void setId(int order_id) {
+    this.id = order_id;
+  }
 
-	public void setId(int order_id) {
-		this.id = order_id;
-	}
+  public Date getDate() {
+    return date;
+  }
 
-	public Date getDate() {
-		return date;
-	}
+  public void setDate(Date date) {
+    this.date = date;
+  }
 
-	public void setDate(Date date) {
-		this.date = date;
-	}
+  public Set<ItemOrderEntity> getItemOrders() {
+    return itemOrders;
+  }
 
-	public Set<ItemOrderEntity> getItemOrders() {
-		return itemOrders;
-	}
+  public void setItemOrders(Set<ItemOrderEntity> itemOrders) {
+    this.itemOrders = itemOrders;
+  }
 
-	public void setItemOrders(Set<ItemOrderEntity> itemOrders) {
-		this.itemOrders = itemOrders;
-	}
+  public UserEntity getUser() {
+    return user;
+  }
 
-	public UserEntity getUser() {
-		return user;
-	}
-
-	public void setUser(UserEntity user) {
-		this.user = user;
-	}
+  public void setUser(UserEntity user) {
+    this.user = user;
+  }
 }
